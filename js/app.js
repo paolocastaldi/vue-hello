623be83347cb2e76e7949b2895b745ff23d84c28
paolocastaldi <pao.gamer97@gmail.com>
2023-02-17 16:19:18 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            message: 'Hello Vue!',
+            image: "./img/batman-keaton-2023"
+        }
+    }
+});
+
+
+app.mount("#root");
